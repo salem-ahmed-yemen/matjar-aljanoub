8f863595257275af9a848f787c45527655f5b528
salem-ahmed-yemen <ebrahimbashmlh@gmail.com>
2026-09-10 19:04:22 +0300

    Add AppTheme class with light theme configuration

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,1 +1,12 @@
 
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get light => ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.indigo,
+    ),
+    fontFamily: 'Arial',
+  );
+}
