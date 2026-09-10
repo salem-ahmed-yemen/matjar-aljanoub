5e919649bd08bcd85dcd0ab8ca6c42a0aa2828f3
salem-ahmed-yemen <ebrahimbashmlh@gmail.com>
2026-09-10 19:10:41 +0300

    Add FirebaseService for Firebase initialization

---
+++ lib/core/services/firebase_service.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseService {
+  static Future<void> initialize() async {
+    await Firebase.initializeApp();
+  }
+
+  static FirebaseApp get app => Firebase.app();
+}
