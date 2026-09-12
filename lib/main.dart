43162026ad277773e4a902107109cb782fbaabf7
salem-ahmed-yemen <ebrahimbashmlh@gmail.com>
2026-09-12 19:05:19 +0300

    Improve formatting of main function in main.dart
    Refactor main function for better readability.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'app.dart';
-Future<void> main() async { WidgetsFlutterBinding.ensureInitialized(); await Firebase.initializeApp(); runApp(const MatjarAlJanoubApp()); }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  runApp(
+    const MatjarAlJanoubApp(),
+  );
+}
